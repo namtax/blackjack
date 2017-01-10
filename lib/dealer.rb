41f2b5897b0d99f5d4de3c7d118a5e83342995a9
Ike Nwaogwugwu <namtax@hotmail.com>
2017-01-10 22:33:45 +0000

    dealer: strip down interface

--- lib/dealer.rb
+++ lib/dealer.rb
@@ -1,42 +1,17 @@
-require 'card_player'
-
 class Dealer 
   include CardPlayer
-  attr_reader :card, :suit, :hand
-
-  MAX_FIRST_HAND = 20
-
-  def initialize(first_card = Card.new, second_card = Card.new)
-    @first_card  = first_card
-    @second_card = second_card
-    set_initial_hand(@first_card, @second_card)
-  end
+  attr_reader :card, :hand
 
   def set_hand
-    if total >= MAX_FIRST_HAND
-      while total >= MAX_FIRST_HAND
-        set_initial_hand(Card.new, Card.new)
-      end
-    end
+    @hand = [Card.new, Card.new]
   end
 
   def hit
     return 'Please deal hand' unless hand
-
     hand << Card.new
 
     while total < 17
       hand << Card.new
     end
-  end
-
-  def total
-    hand.map(&:number).inject(:+)
-  end
-  
-  private
-
-  def set_initial_hand(first_card, second_card)
-    @hand  = [first_card, second_card]
   end
 end
--- spec/lib/dealer_spec.rb
+++ spec/lib/dealer_spec.rb
@@ -1,87 +1,50 @@
 require 'spec_helper'
 
 describe Dealer do
-  let(:dealer)        { Dealer.new(first_card, second_card) }
-  let!(:first_card)   { Card.new(14, 'hearts') }
-  let!(:second_card)  { Card.new(6,  'hearts') }
-  let(:cards)         { dealer.cards }
-
-  describe '#initialize' do 
-    context 'when sum of first 2 cards value > 20' do 
-      it 'adds first cards to dealers hand' do 
-        expect(dealer.hand).to eq([first_card, second_card])
-      end
-    end
+  let!(:first_card)   { Card.new(10, 'hearts') }
+  let!(:second_card)  { Card.new(2,  'hearts') }
+  let(:cards)         { [first_card, second_card] }
+  
+  before do 
+    allow(Card).to receive(:new).and_return(*cards)
   end
 
   describe '#set_hand' do 
-    context 'when sum of first 2 cards value >=20' do 
-      let!(:first_redealt_card)  { Card.new(15, 'hearts') }
-      let!(:second_redealt_card) { Card.new(5,  'hearts') }
-      let!(:third_redealt_card)  { Card.new(11, 'hearts') }
-      let!(:fourth_redealt_card) { Card.new(8,  'hearts') }
-
-      before do 
-        expect(Card).to receive(:new)
-          .and_return(first_redealt_card, second_redealt_card, third_redealt_card, fourth_redealt_card)
-      end
-
-      it 'redeals the cards until cards value > 20' do 
-        dealer.set_hand
-        expect(dealer.hand).to match_array([third_redealt_card, fourth_redealt_card])
-      end
+    it 'adds first cards to dealers hand' do 
+      subject.set_hand
+      expect(subject.hand).to eq(cards)
     end
   end
 
   describe '#hit' do 
     context 'dealers hand is >= 17 after first hit' do 
-     let!(:first_card)  { Card.new(10, 'hearts') }
-     let!(:second_card) { Card.new(2, 'hearts') }
-     let!(:third_card)  { Card.new(4, 'hearts') }
-     let!(:fourth_card) { Card.new(10, 'hearts') }
+      let!(:third_card)  { Card.new(4, 'hearts') }
+      let!(:fourth_card) { Card.new(10, 'hearts') }
+      let(:cards)        { [first_card, second_card, third_card, fourth_card]}
 
-     before do 
-       dealer.set_hand
-       expect(Card).to receive(:new).and_return(third_card, fourth_card)
-     end
+      before { subject.set_hand }
 
-     it 'adds another card to dealers hand' do 
-       dealer.hit
-       expect(dealer.hand).to eq([first_card, second_card, third_card, fourth_card])
-     end
+      it 'adds another card to dealers hand' do 
+        subject.hit
+        expect(subject.hand).to eq(cards)
+      end
     end
 
     context 'dealers hand is > 17 after first hit' do 
-     let(:first_card)  { Card.new(10, 'hearts') }
-     let(:second_card) { Card.new(2, 'hearts') }
-     let(:third_card)  { Card.new(6, 'hearts') }
+      let(:third_card)  { Card.new(6, 'hearts') }
+      let(:cards)       { [first_card, second_card, third_card] }
 
-      before do 
-        dealer.set_hand
-        expect(Card).to receive(:new).and_return(third_card)
-      end
+      before { subject.set_hand }
 
       it 'does not add another card to dealers hand' do 
-        dealer.hit
-        expect(dealer.hand.size).to eq(3)
-        expect(dealer.total >= 17).to be_truthy
+        subject.hit
+        expect(subject.hand).to eq(cards)
+        expect(subject.total >= 17).to be_truthy
       end
     end
 
-    context 'dealers hand is > 21' do 
-     let!(:first_card)          { Card.new(8, 'hearts') }
-     let!(:second_card)         { Card.new(10, 'hearts') }
-     let!(:dealers_first_hit)   { Card.new(10, 'hearts') }
-
-     before do 
-       dealer.set_hand
-       allow(Card).to receive(:new).and_return(dealers_first_hit)
-     end
-
-     it 'marks player as loser' do 
-       outcome = dealer.hit
-       expect(dealer).to be_lost
-     end
+    context 'dealers hand empty' do 
+      it { expect(subject.hit).to eq('Please deal hand') }
     end
   end
 end
