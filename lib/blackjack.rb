36f612b54c12ae459b8e2aa78dff683e1506f613
Ike Nwaogwugwu <namtax@hotmail.com>
2017-01-10 22:45:34 +0000

    blackjack: allow interaction via cli

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,2 +1,33 @@
+require './lib/card_player'
+require './lib/game'
+require './lib/card'
+require './lib/player'
+require './lib/dealer'
+
 class Blackjack
+  def run
+    game = Game.new
+    game.start
+
+    puts game.response
+     
+    while open
+      input  = gets.chomp
+      game.player_requests(input)
+
+      puts game.response
+
+      if game.over?
+        end_game
+      end
+    end
+  end
+
+  def open
+    @end.nil?
+  end
+
+  def end_game
+    @end = true
+  end
 end
